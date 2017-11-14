d2d557874c8be3af590d5b617125df36659c43cf
chenlj <chenljscu@gmail.com>
2017-11-14 01:21:21 +0000

    plot

--- macros/resDistribution.c
+++ macros/resDistribution.c
@@ -6,8 +6,10 @@
 
 void resDistribution(){
 
-  TH1F* hdx = new TH1F("hdx","hdx",100,-0.05,0.05);
-  TH1F* hdy = new TH1F("hdy","hdy",100,-0.05,0.05);
+  TH1F* hdx = new TH1F("hdx","hdx",200,-0.05,0.05);
+  TH1F* hdy = new TH1F("hdy","hdy",200,-0.05,0.05);
+  TH2F* digiHisHit = new TH2F("digiHisHit","Digital Hit Map",200,-1,1,200,-1,1);
+  TH2F* truthHisHit = new TH2F("truthHisHit","Truth Hit Map",200,-1,1,200,-1,1);
 
   string line;
   int evtId,hitId,chipId,HitNofDigi,TrNofDigi;
@@ -20,13 +22,15 @@ void resDistribution(){
     fin>>evtId>>hitId>>chipId>>posX>>posY>>hitADC>>HitNofDigi>>trPosX>>trPosY>>edep>>TrNofDigi;
     fin.seekg(1,ios::cur);
     if(!fin.good()) break;
-    
+
     //cout<<" posX: "<<posX<<" trPosX: "<<trPosX<<endl;
     double dx = posX-trPosX;
     double dy = posY-trPosY;
 
     hdx->Fill(dx);
     hdy->Fill(dy);
+    digiHisHit->Fill(posX,posY);
+    truthHisHit->Fill(trPosX,trPosY);
   }
 
   TCanvas* c1 = new TCanvas("c1","c1",1440,720);
@@ -37,4 +41,10 @@ void resDistribution(){
   c1->cd(2);
   hdy->Draw();
   //hdy->Fit("gaus");
+  TCanvas* c2 = new TCanvas("c2","c2",1440,720);
+  c2->Divide(2,1);
+  c2->cd(1);
+  digiHisHit->Draw("colz");
+  c2->cd(2);
+  truthHisHit->Draw("colz");
 }
